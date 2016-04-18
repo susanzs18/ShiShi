686f1741b2ad2bb5a354f1c07d6a22181eefd510
lanou <lanou@s.local>
2016-04-18 20:05:09 +0800

    xiugai

--- Hello/Hello/ViewController.m
+++ Hello/Hello/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"lala");
+    NSLog(@"lala11111");
 }
 
 - (void)didReceiveMemoryWarning {
